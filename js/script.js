3ddd0207e41bc8c909bccb7695fd5ef21486e498
ayatakeshi <86645120+ayatakeshi@users.noreply.github.com>
2023-09-09 14:32:01 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -51,4 +51,26 @@ newsList.forEach(element => {
     }
 });
 
+$('.p-slider').slick({
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+        breakpoint: 1025,//モニターの横幅が769px以下の見せ方
+        settings: {
+            slidesToShow: 3,//スライドを画面に2枚見せる
+            slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
+        }
+        },
+        {
+            breakpoint: 768,//モニターの横幅が426px以下の見せ方
+            settings: {
+                slidesToShow: 1.5,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+            }
+        }
+    ]
+});
 
